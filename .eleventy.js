a8ca48f0f35d06399fdde1704705587afeca36fe
Christopher Hannah <me@chrishannah.me>
2025-09-01 20:26:12 +0100

    add reading time

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 const { feedPlugin } = require("@11ty/eleventy-plugin-rss");
 
+const readingTime = require('eleventy-plugin-reading-time');
+
 module.exports = function (eleventyConfig) {
 
   /* Assets */
@@ -16,21 +18,39 @@ module.exports = function (eleventyConfig) {
 
   /* Feeds */
   eleventyConfig.addPlugin(feedPlugin, {
-		type: "rss",
-		outputPath: "/feed.xml",
-		collection: {
-			name: "posts",
-			limit: 10,
-		},
-		metadata: {
-			language: "en",
-			title: "chrishannah.dev",
-			base: "https://chrishannah.dev",
-			author: {
-				name: "Chris Hannah",
-			}
-		}
-	});
+    type: "rss",
+    outputPath: "/feed.xml",
+    collection: {
+      name: "posts",
+      limit: 10,
+    },
+    metadata: {
+      language: "en",
+      title: "chrishannah.dev",
+      base: "https://chrishannah.dev",
+      author: {
+        name: "Chris Hannah",
+      }
+    }
+  });
+
+  /* Plugins */
+  eleventyConfig.addPlugin(readingTime);
+
+  /* Debug */
+  eleventyConfig.addFilter('dump', obj => {
+    const getCircularReplacer = () => {
+      const seen = new WeakSet();
+      return (key, value) => {
+        if (typeof value === "object" && value !== null) {
+          if (seen.has(value)) return;
+          seen.add(value);
+        }
+        return value;
+      };
+    };
+    return JSON.stringify(obj, getCircularReplacer(), 2);
+  });
 
   return {
     dir: {
